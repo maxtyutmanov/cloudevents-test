640e96aa1e0a4b1ab8a6512d2af5cace792b237e
Max Tiutmanov <max.tiutmanov@bookedby.com>
2023-01-29 19:36:22 +0300

    extracted consumer to a separate .net framework app

---
+++ CloudEvents/CloudEvents.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using CloudEvents.Shared;
+using CloudNative.CloudEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudEvents.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var broker = new Broker("activemq:tcp://localhost:61616"))
+            using (var subscription = broker.Subscribe<GitRepositorySizeChangeInfo>(CloudEventSources.GitService, CloudEventTypes.GitRepositorySizeChanged, Handler))
+            {
+                Console.WriteLine("Consumer is listening... Press enter to stop the consumer.");
+                Console.ReadLine();
+            };
+        }
+
+        static async Task Handler(CloudEvent ce, GitRepositorySizeChangeInfo data)
+        {
+            Console.WriteLine(
+                    "Received repository size change notification. ProjectId = {0}, repository size = {1}, size status = {2}",
+                    data.ProjectId, data.RepositorySizeInMb, data.SizeStatus);
+        }
+    }
+}
--- CloudEvents/CloudEvents.Producer/Program.cs
+++ CloudEvents/CloudEvents.Producer/Program.cs
@@ -2,13 +2,6 @@
 using CloudEvents.Shared;
 
 using var broker = new Broker("activemq:tcp://localhost:61616");
-using var subscription = broker.Subscribe<GitRepositorySizeChangeInfo>(CloudEventSources.GitService, CloudEventTypes.GitRepositorySizeChanged, async (_, data) =>
-{
-    Console.WriteLine(
-        "Received repository size change notification. ProjectId = {0}, repository size = {1}, size status = {2}",
-        data.ProjectId, data.RepositorySizeInMb, data.SizeStatus);
-});
-
 await broker.Publish(CloudEventSources.GitService, CloudEventTypes.GitRepositorySizeChanged, new GitRepositorySizeChangeInfo
 {
     ProjectId = "project-1",
